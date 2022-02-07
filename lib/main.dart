e4c8779971c2c49f64432d95d2c4a7d83e8845e2
Soniya V <soniyavijaymanohar@gmail.com>
2022-02-07 20:02:17 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String url = "https://www.currentscience.ac.in/data/forthcoming/349.pdf";
+  late PDFDocument _doc;
+  late bool _loading;
+  @override
+  void initState() {
+    super.initState();
+    _initPdf();
+  }
+
+  /*git init
+  git add .
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/soniyav20/pdf_viewer.git
+  git push -u origin main*/
+  _initPdf() async {
+    setState(() {
+      _loading = true;
+    });
+    final doc = await PDFDocument.fromURL(url);
+    setState(() {
+      _doc = doc;
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My PDFviewer"),
+        centerTitle: true,
+      ),
+      body: _loading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : PDFViewer(
+              document: _doc,
+            ),
+    );
+  }
+}
